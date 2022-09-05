628b0fa872aa95277fa1aac2f1afcdd49b05a518
Inon Reuven <inonreu@gmail.com>
2022-09-05 19:23:55 +0300

    1st

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import numpy as np
+import torch
+import torchvision
+from torchvision import transforms, datasets
+
 
 def main():
-    print("hi inon")
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,)),
+                                    ])
+    trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
+    valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+    valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 
 if __name__ == '__main__':
     main()
